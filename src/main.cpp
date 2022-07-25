ddc8d82a6b480ecf58e2f707b803fc71467f6564
octal-ip <hens.john@gmail.com>
2022-07-25 11:40:51 +1000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@
 /*
 #define SECRET_SSID "<ssid>>"
 #define SECRET_PASS "<password>"
-#define SECRET_INFLUXDB "http://10.2.2.155:8086/write?db=<db name>&u=<user name>&p=<password>"
+#define SECRET_INFLUXDB "http://<IP address>:8086/write?db=<db name>&u=<user name>&p=<password>"
 */
 
 
