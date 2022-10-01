41746eee46b2826b1f055a65bd3304d1c50b6810
octal-ip <hens.john@gmail.com>
2022-10-01 21:51:18 +1000

    Update secrets.h

--- include/secrets.h
+++ include/secrets.h
@@ -1,3 +1,8 @@
 #define SECRET_SSID "<ssid>>"
 #define SECRET_PASS "<password>"
-#define SECRET_INFLUXDB "http://<IP address>:8086/write?db=<db name>&u=<user name>&p=<password>"
+#define SECRET_INFLUXDB "http://<IP address>:8086/write?db=<db name>&u=<username>&p=<password>"
+#define SECRET_INFLUX_IP_OCTET1 10
+#define SECRET_INFLUX_IP_OCTET2 1
+#define SECRET_INFLUX_IP_OCTET3 1
+#define SECRET_INFLUX_IP_OCTET4 200
+#define SECRET_MQTT_SERVER "10.1.1.200"
