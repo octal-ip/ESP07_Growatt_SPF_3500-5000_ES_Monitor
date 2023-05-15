af40fe3ca06ec263f4dbce7aab0538328dab93c9
octal-ip <hens.john@gmail.com>
2023-05-16 07:20:46 +1000

    Update secrets.h

--- include/secrets.h
+++ include/secrets.h
@@ -1,4 +1,4 @@
-#define SECRET_SSID "<ssid>>"
+#define SECRET_SSID "<ssid>"
 #define SECRET_PASS "<password>"
 #define SECRET_INFLUXDB "http://<IP address>:8086/write?db=<db name>&u=<username>&p=<password>"
 #define SECRET_INFLUX_IP_OCTET1 10
